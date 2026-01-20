c048ed9de25d0d6babc01bc618cd7836925a0e1e
hadienzz <hadinpramiadi@gmail.com>
2026-01-20 15:17:45 +0700

    save local change before pulling

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,24 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/error.middleware";
 import authRoutes from "./feat/auth/auth.route";
+
 import cors from "cors";
+
 const app = express();
 
 app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
 app.use(cookieParser());
 
+// API Routes
 app.use("/api/auth", authRoutes);
 
+// Error handler (must be last)
 app.use(errorHandler);
 
 export default app;
